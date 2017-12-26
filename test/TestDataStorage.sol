3bf95815136e20a1a3e414af02aa317c24273191
Kevin Wu <kevin.wu@ucsb.edu>
2017-12-26 10:10:18 -0800

    rename test to test_insertOne

--- test/TestDataStorage.sol
+++ test/TestDataStorage.sol
@@ -10,7 +10,7 @@ contract TestDataStorage {
   address acct2 = 0xf17f52151EbEF6C7334FAD080c5704D77216b732;
   address acct3 = 0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef;
 
-  function test_loadedUp() public {
+  function test_insertOne() public {
     /*DataStorage ds = DataStorage(DeployedAddresses.DataStorage());*/
     DataStorage ds = new DataStorage();
 
